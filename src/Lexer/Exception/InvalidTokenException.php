6e734bbae4546280764a7cc107dfaf80b7e6bc73
Walter Tamboer <walter@tamboer.nl>
2017-10-13 23:46:59 +0200

    Updated the license reference

--- src/Lexer/Exception/InvalidTokenException.php
+++ src/Lexer/Exception/InvalidTokenException.php
@@ -4,7 +4,7 @@
  *
  * @link https://github.com/chesszebra/portable-game-notation for the canonical source repository
  * @copyright Copyright (c) 2017 Chess Zebra (https://chesszebra.com)
- * @license https://github.com/chesszebra/portable-game-notation/blob/master/LICENSE MIT
+ * @license https://github.com/chesszebra/portable-game-notation/blob/master/LICENSE.md MIT
  */
 
 namespace ChessZebra\PortableGameNotation\Lexer\Exception;
